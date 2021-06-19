3f8680263aae7b1ba9a9962c0d80755a349beb41
branyoe <bhernandez1@ucol.mx>
2021-06-19 18:29:14 -0500

    finalización del primer testeo

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -7,7 +7,7 @@ import Role from '../models/Roles';
 export const createUser = async (req, res) => {
   try {
     const { username, email, password, roles } = req.body;
-    if(!username || !email || !password) return res.status(401).json({message: "incomplet request"});
+    if (!username || !email || !password) return res.status(401).json({ message: "incomplet request" });
 
     //esquema de nuevo usuario
     const newUser = new User({
@@ -17,36 +17,36 @@ export const createUser = async (req, res) => {
     })
 
     //si existen roles recividos busca el rol en la base de datos segun su "name" y lo sustituye por su _id
-    if(roles){
-      if(roles.length === 1 && roles[0] === 'admin'){
+    if (roles) {
+      if (roles.length === 1 && roles[0] === 'admin') {
         roles.push('moderator');
-        const foundRole = await Role.find({name: {$in: roles}});
+        const foundRole = await Role.find({ name: { $in: roles } });
         newUser.roles = foundRole.map(role => role._id);
-      }else{
-        const foundRole = await Role.find({name: {$in: roles}})
+      } else {
+        const foundRole = await Role.find({ name: { $in: roles } })
         newUser.roles = foundRole.map(role => role._id)
       }
-    }else{//si no lo encuentra pone el rol "user" por defecto
-      const role = await Role.findOne({name: "user"});
+    } else {//si no lo encuentra pone el rol "user" por defecto
+      const role = await Role.findOne({ name: "user" });
       newUser.roles = [role._id];
     }
 
     //guarda usuario en db
     const savedUser = await newUser.save();
     //crea un token
-    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+    const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
       expiresIn: 259200 //3 dias 
     })
-    
+
     //responde con el token 
-    res.status(200).json({token});
+    res.status(200).json({ token });
   } catch (e) {
     res.status(403).json(e);
   }
 }
 
 //get all Users
-export const getUsers = async(req, res) => {
+export const getUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
@@ -56,11 +56,11 @@ export const getUsers = async(req, res) => {
 }
 
 //get users by id
-export const getUserById = async(req, res) => {
+export const getUserById = async (req, res) => {
   try {
-    if(!req.params.userId) res.status(403).json({message: "id not received"})
+    if (!req.params.userId) res.status(403).json({ message: "id not received" })
     const user = await User.findById(req.params.userId);
-    if(!user) return res.status(403).json({message: "a user related to the id was not found"})
+    if (!user) return res.status(403).json({ message: "a user related to the id was not found" })
     res.status(200).json(user);
   } catch (e) {
     res.status(403).json(e);
@@ -68,41 +68,68 @@ export const getUserById = async(req, res) => {
 }
 
 //update a user by id
-export const updateUserById = async(req, res) => {
+export const updateUserById = async (req, res) => {
   try {
-      //datos recividos
+    console.log('entró');
+    //Validaciones**********************************
+    //valida userId
+    if (!req.params.userId) return res.status(403).json({ message: "userId not provied" });
+    //valida req.body
+    if (!req.body) return res.status(403).json({ message: "incoplet request" });
+
+    //DEstructuraciones*****************************
+    //destructuración de req.body
     const { username, email, password, roles } = req.body;
-    if(!username || !email || !password) return res.status(403).json({message: "incoplet request"});
+    //destructuración de userID
+    const userId = req.params.userId;
+
+    //Diseño del usuario actualizado****************
+    //Referencia a usuario viejo
+    const oldUser = await User.findById(userId);
     //esquema de nuevo usuario
     const updatedUser = {
-      username,
-      email,
-      password: await User.encryptPassword(password),
-      roles
+      username: username ? username : oldUser.username,
+      email: email ? email : oldUser.email,
+      password: password ? await User.encryptPassword(password) : oldUser.password
+      // roles: roles ? roles : oldUser.roles
     }
 
-    if(roles){
-      const foundRole = await Role.find({name: {$in: roles}})
-      updatedUser.roles = foundRole.map(role => role._id)
-    }else{//si no lo encuentra pone el rol "user" por defecto
-      const role = await Role.findOne({name: "user"});
-      updatedUser.roles = [role._id];
+    //Verificación y pusheo de roles************************
+
+    if (roles) {
+      if (roles.length === 1 && roles[0] === 'admin') {
+        roles.push('moderator');
+        const foundRole = await Role.find({ name: { $in: roles } });
+        updatedUser.roles = foundRole.map(role => role._id);
+      } else {
+        if (roles.length === 0) {
+          console.log('entroó aquí');
+          updatedUser.roles = oldUser.roles
+        } else {
+          const foundRole = await Role.find({ name: { $in: roles } })
+          updatedUser.roles = foundRole.map(role => role._id)
+        }
+      }
+    } else {//si no lo encuentra pone el rol "user" por defecto
+      const role = oldUser.roles;
+      updatedUser.roles = role;
     }
 
-    const user = await User.findByIdAndUpdate(req.params.userId, updatedUser,{
+    //Actualización de usuario
+    const user = await User.findByIdAndUpdate(userId, updatedUser, {
       new: true
-    })
-
+    });
+    //respuesta
     res.status(200).json(user);
   } catch (e) {
-    res.status(403).json(e); 
+    res.status(403).json(`entró ${e}`);
   }
 }
 
 //delete a user by id
-export const deleteUserById = async(req, res) => {
+export const deleteUserById = async (req, res) => {
   try {
-    if(!req.params.userId) return res.status(403).json({message: "id not received"})
+    if (!req.params.userId) return res.status(403).json({ message: "id not received" })
     await User.findByIdAndDelete(req.params.userId);
     res.status(204).json();
   } catch (e) {
