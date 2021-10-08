b01eab2fd0ed2baed0057b442aaf6ad9da16899e
branyoe <bhernandez1@ucol.mx>
2021-10-08 12:27:41 -0500

    s

--- src/data-base.js
+++ src/data-base.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 mongoose.connect('mongodb+srv://branyoe:1003@cluster0.ijqta.mongodb.net/ssc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
+    // useFindAndModify: true,
+    // useCreateIndex: true
 })
 .then(db => console.log('DB is connected'))
 .catch(error => console.log(error))
